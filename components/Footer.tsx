fd23b5d51bc24c2edd9367f8ad63ab5792fa0837
anangfirmansyah <anangfirmansyahp5@gmail.com>
2023-09-07 14:29:44 +0700

    fix: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
 	return (
 		<footer className='bg-white border-t'>
 			<div className='mx-auto py-10'>
-				<p className='text-center text-xs text-black'>&copy; 2023 FakestoreName, Inc, All rights reserverd.</p>
+				<p className='text-center text-xs text-black'>&copy; 2023 Nusa Store, Inc, All rights reserverd.</p>
 			</div>
 		</footer>
 	);
